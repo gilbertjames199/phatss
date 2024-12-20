299a34bf828fa8e48ba8e1c607c6f7709a9246f7
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2024-12-20 09:53:31 +0800

    comment

--- resources/js/Pages/Respondent/Index.vue
+++ resources/js/Pages/Respondent/Index.vue
@@ -13,20 +13,81 @@
                 <thead>
                     <tr class="bg-secondary text-white">
                         <th></th>
-                        <th>Name</th>
+                        <th>LGU</th>
                         <th>Office</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr >
                         <td></td>
-                        <td>Jerry Cans</td>
-                        <td>Provincial Health Office</td>
+                        <td>Compostela</td>
+                        <td>4 RSIs</td>
                     </tr>
                     <tr >
                         <td></td>
-                        <td>Juan Dela Cruz</td>
-                        <td>Municipal Health Office</td>
+                        <td>New Bataan</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Maragusan</td>
+                        <td>2 RSIs</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Montevista</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Monkayo</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Monkayo</td>
+                        <td>1 RSI</td>
+                    </tr>
+
+                    <tr >
+                        <td></td>
+                        <td>Nabunturan</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Pantukan</td>
+                        <td>2 RSIs</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Mabini</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Maco</td>
+                        <td>2 RSIs</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Mawab</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Laak</td>
+                        <td>1 RSI</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Maco</td>
+                        <td>37 BVSIs</td>
+                    </tr>
+                    <tr >
+                        <td></td>
+                        <td>Pantukan</td>
+                        <td>17 BVSIs</td>
                     </tr>
                 </tbody>
             </table>
