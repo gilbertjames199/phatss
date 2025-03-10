0c6b34635f5bcc83326123b6de2c4c63caedb976
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2025-03-10 10:38:52 +0800

    comment

--- resources/js/Shared/Sidebar.vue
+++ resources/js/Shared/Sidebar.vue
@@ -225,7 +225,7 @@
                                 <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2z"/>
                             </svg>
                         </span>
-                        <span class="title">Sanitation Best Practices</span>
+                        <span class="title">Sanitation Insights</span>
                     </Link>
                 </li>
                 <!--Users-->
