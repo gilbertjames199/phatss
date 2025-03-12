610fef8604f603eb237c556469361b57b642bc91
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2025-03-12 08:04:15 +0800

    comment

--- app/Http/Controllers/MapPlotterController.php
+++ app/Http/Controllers/MapPlotterController.php
@@ -210,7 +210,7 @@ class MapPlotterController extends Controller
                 return [
                     'x' => (float) $item->_Location_latitude,
                     'y' => (float) $item->_Location_longitude,
-                    'count' => $count,
+                    'count' =>  $item->cluster_size,
                     'name' => $item->LAST_NAME . ', ' . $item->FIRST_NAME . ' ' . $item->MIDDLENAME,
                     'address' => 'purok-' . $item->purok_sitio . ', ' . $item->barangay,
                     'precision' => $item->_Location_precision,
